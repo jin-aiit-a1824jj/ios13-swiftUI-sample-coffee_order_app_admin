2a1bb539f7a49c29a306be63619eb1d6887ac907
a1824jj <a1824jj@aiit.ac.jp>
2020-04-15 18:16:25 +0900

    import class

---
+++ CoffeeOrder_Admin_sampel/Model/Order.swift
@@ -0,0 +1,51 @@
+//
+//  Order.swift
+//  CoffeeOrder_sampel
+//
+//  Created by JONGWOO JIN on 2020/04/15.
+//  Copyright © 2020 JONGWOO JIN. All rights reserved.
+//
+
+import Foundation
+
+class Order: Identifiable {
+    
+    var id = ""
+    var customerId = ""
+    var orderItems: [Drink] = []
+    var amount = 0.0
+    
+    var customerName = ""
+    var isCompleted = false
+    
+    func saveOrderToFirestore() {
+        FirebaseReference(.Order).document(self.id).setData(orderDictionaryFrom(self)) { error in
+            if error != nil {
+                print("error saving order to firestore: ", error!.localizedDescription)
+            }
+        }
+    }
+    
+}
+
+func orderDictionaryFrom(_ order: Order) -> [String : Any] {
+    var allDrinkIds: [String] = []
+    
+    for drink in order.orderItems {
+        allDrinkIds.append(drink.id)
+    }
+    
+    return NSDictionary(objects:[order.id,
+                                   order.customerId,
+                                   allDrinkIds,
+                                   order.amount,
+                                   order.customerName,
+                                   order.isCompleted],
+                        forKeys:[ kID as NSCopying,
+                                       kCUSTOMERID as NSCopying,
+                                       kDRINKIDS as NSCopying,
+                                       kAMOUNT as NSCopying,
+                                       kCUSTOMERID as NSCopying,
+                                       kISCOMPLETED as NSCopying
+    ]) as! [String : Any]
+}
